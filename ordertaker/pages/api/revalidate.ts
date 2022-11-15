3ea90cb7a877ab4fb0fb5443a34a31e5b5035ab9
Mr.Cha <cjy4044@gmail.com>
2022-11-15 14:52:05 +0900

    revalidate수정

--- ordertaker/pages/api/revalidate.ts
+++ ordertaker/pages/api/revalidate.ts
@@ -1,20 +1,21 @@
-/**
- * https://www.paigeniedringhaus.com/blog/how-to-unit-test-next-js-api-routes-with-typescript
- */
- import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
- export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-   // 유효한 요청인지부터 확인
-   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
-     return res.status(401).json({ message: '유효하지 않은 토큰' });
-   }
- 
-   try {
-     // 페이지 재생성
-     await res.unstable_revalidate('/');
-     return res.json({ revalidated: true });
-   } catch (err) {
-     // 만약 에러가 있다면, Next.js는 기존에 성공적으로 생성되어있던 페이지를 보여줄 것이다.
-     return res.status(500).send('유효성 재검증 에러');
-   }
- }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
+  try {
+    // Regenerate our index route showing the images
+    await res.revalidate('/')
+    return res.json({ revalidated: true })
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating')
+  }
+}
