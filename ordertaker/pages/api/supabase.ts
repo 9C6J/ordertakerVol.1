aacbbccdeb4b67cc94a3323feac88e9aefe021f4
Mr.Cha <cjy4044@gmail.com>
2023-05-08 11:07:12 +0900

    test

--- ordertaker/pages/api/supabase.ts
+++ ordertaker/pages/api/supabase.ts
@@ -2,15 +2,15 @@ import { createClient } from '@supabase/supabase-js';
 
 // Supabase client
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-// const supabaseToken = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
 const supabaseServiceToken = process.env.NEXT_PUBLIC_SERVICE_KEY || "";
-const supabaseServiceAnonToken = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient(
   supabaseUrl as string,
-  supabaseServiceAnonToken as string,
+  supabaseServiceToken as string,
 );
 
-export const supabaseClient = createClient(supabaseUrl, supabaseServiceAnonToken)
+debugger;
+
+export const supabaseClient = createClient(supabaseUrl, supabaseServiceToken)
 
 
